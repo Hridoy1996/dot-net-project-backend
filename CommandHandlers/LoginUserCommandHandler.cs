cc9eba9f1aba50bcebc370dd0e6aecd33f455caf
Tareq Mahmud Hridoy <31369790+Hridoy1996@users.noreply.github.com>
2022-11-01 20:42:17 +0600

    //

--- CommandHandlers/LoginUserCommandHandler.cs
+++ CommandHandlers/LoginUserCommandHandler.cs
@@ -1,22 +1,22 @@
-﻿using Command.UAM;
+﻿
 using Contract;
 
 using MediatR;
 
 namespace CommandHandlers
 {
-    public class LoginUserCommandHandler : AsyncRequestHandler<LoginUserCommand>
-    {
-        private IUserManagerServices _userManagerServices;
-        public LoginUserCommandHandler(IUserManagerServices userManagerServices)
-        {
-            _userManagerServices = userManagerServices;
-        }
-        protected override Task Handle(LoginCommand request, CancellationToken cancellationToken)
-        {
-            await _userManagerServices.Login(request.UserName, request.Password);
+    //public class LoginUserCommandHandler : AsyncRequestHandler<LoginUserCommand>
+    //{
+    //    private IUserManagerServices _userManagerServices;
+    //    public LoginUserCommandHandler(IUserManagerServices userManagerServices)
+    //    {
+    //        _userManagerServices = userManagerServices;
+    //    }
+    //    protected override Task Handle(LoginCommand request, CancellationToken cancellationToken)
+    //    {
+    //        await _userManagerServices.Login(request.UserName, request.Password);
            
-            return Task.CompletedTask;
-        }
-    }
+    //        return Task.CompletedTask;
+    //    }
+    //}
 }
--- QueryHandler/LoginUserCommandHandler.cs
+++ QueryHandler/LoginUserCommandHandler.cs
@@ -5,19 +5,19 @@ using Queries.UAM;
 
 namespace QueryHandler
 {
-    public class LoginUserHandler : IRequestHandler<LoginQuery, string>
-    {
-        private IUserManagerServices _userManagerServices;
-        public LoginUserHandler(IUserManagerServices userManagerServices)
-        {
-            _userManagerServices = userManagerServices;
-        }
-        //protected override async Task Handle(LoginQuery request, CancellationToken cancellationToken)
-        //{
-        //    await _userManagerServices.Login(request.UserName, request.Password);
+    //public class LoginUserHandler : IRequestHandler<LoginQuery, string>
+    //{
+    //    private IUserManagerServices _userManagerServices;
+    //    public LoginUserHandler(IUserManagerServices userManagerServices)
+    //    {
+    //        _userManagerServices = userManagerServices;
+    //    }
+    //    //protected override async Task Handle(LoginQuery request, CancellationToken cancellationToken)
+    //    //{
+    //    //    await _userManagerServices.Login(request.UserName, request.Password);
            
-        //    return Task.CompletedTask;
-        //}
+    //    //    return Task.CompletedTask;
+    //    //}
 
-    }
+    //}
 }

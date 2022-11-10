0091c4c78c92db4fb0ecfcb544eac829eaa78760
Tareq Mahmud Hridoy <31369790+Hridoy1996@users.noreply.github.com>
2022-11-10 23:19:03 +0600

    set otp text message

--- CommandHandlers/Sms/OtpRequestCommandHandler.cs
+++ CommandHandlers/Sms/OtpRequestCommandHandler.cs
@@ -29,7 +29,8 @@ namespace CommandHandlers.Sms
 
             var addOtpKeyTask = _keyStore.AddKeyWithExpiryAsync($"TelemedicineOtp_{request.MobileNumber}", hashedOtp, (1000 * 60 * 2) + (1000 * 30));
 
-            await _smsService.SendTextMessageAsync(request.Otp, request.MobileNumber);
+            string otpMessage = $"Your OTP  is {request.Otp}. Don't share it with anybody!";
+            await _smsService.SendTextMessageAsync(otpMessage, request.MobileNumber);
 
             await addOtpKeyTask;
 

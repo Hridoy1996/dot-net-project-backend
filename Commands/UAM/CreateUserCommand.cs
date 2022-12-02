aaa508d1ad079d48558027d0c1c6454ce1ff471e
Tareq Mahmud Hridoy <31369790+Hridoy1996@users.noreply.github.com>
2022-12-02 18:00:20 +0600

    //

--- Commands/UAM/CreateUserCommand.cs
+++ Commands/UAM/CreateUserCommand.cs
@@ -18,7 +18,7 @@ namespace Commands.UAM
         public string? LastName { get; set; }
         public string? Address { get; set; }
         public string? Email { get; set; }
-        public string Password { get; set; } = null!;
+        public string? Password { get; set; }
         public DateTime DOB { get; set; }
         public string? CountryName { get; set; }
         public string? ProfileImageId { get; set; }
